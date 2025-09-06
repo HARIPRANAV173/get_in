4aab296ae99675e284f200adc61732a4098d9594
Haripranav <143796219+HARIPRANAV173@users.noreply.github.com>
2025-09-06 20:26:23 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+export default defineConfig({
+  plugins: [react(), basicSsl()],
+  optimizeDeps: {
+    exclude: ["lucide-react", "better-sqlite3"],
+  },
+  define: {
+    global: "globalThis",
+  },
+  resolve: {
+    alias: {
+      "better-sqlite3": "better-sqlite3/lib/database.js",
+    },
+  },
+});
